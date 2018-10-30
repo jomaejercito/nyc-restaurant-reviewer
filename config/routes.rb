0476c63c9f654644e809afe13e7ad2d9ee91ccdc
Joma Ejercito <jomaejercito@gmail.com>
2018-10-30 18:12:39 -0400

    Set root path and commented out pre-made routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'neighborhoods/index'
-  get 'neighborhoods/show'
-  get 'cuisines/index'
-  get 'cuisines/show'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/show'
-  get 'reviews/edit'
-  get 'index/new'
-  get 'index/show'
-  get 'sessions/new'
-  get 'welcome/home'
+
+  root 'welcome#home'
+
+  #get 'neighborhoods/index'
+  #get 'neighborhoods/show'
+  #get 'cuisines/index'
+  #get 'cuisines/show'
+  #get 'restaurants/index'
+  #get 'restaurants/show'
+  #get 'reviews/index'
+  #get 'reviews/new'
+  #get 'reviews/show'
+  #get 'reviews/edit'
+  #get 'index/new'
+  #get 'index/show'
+  #get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
