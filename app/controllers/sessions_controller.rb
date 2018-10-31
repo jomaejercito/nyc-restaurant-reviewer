a99403325d889a8ba2b51c35ff310d7011a80aab
Joma Ejercito <jomaejercito@gmail.com>
2018-10-30 22:13:26 -0400

    Set up omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,20 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def facebook
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.password = 'password'
+    end
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
   resources :users do
