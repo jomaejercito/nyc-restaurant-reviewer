bd3b31a6bdfd825988327d8621c61c6cca2ac713
Joma Ejercito <jomaejercito@gmail.com>
2018-10-30 22:00:40 -0400

    Created and edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,26 +9,12 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  resources :users
-  resources :restaurants
-  resources :reviews
+  resources :users do
+    resources :reviews
+  end
 
+  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :cuisines, only: [:index, :show]
+  resources :neighborhoods, only: [:index, :show]
 
-  #get 'users/index'
-  #get 'users/show'
-  #get 'neighborhoods/index'
-  #get 'neighborhoods/show'
-  #get 'cuisines/index'
-  #get 'cuisines/show'
-  #get 'restaurants/index'
-  #get 'restaurants/show'
-  #get 'reviews/index'
-  #get 'reviews/new'
-  #get 'reviews/show'
-  #get 'reviews/edit'
-  #get 'index/new'
-  #get 'index/show'
-  #get 'sessions/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
