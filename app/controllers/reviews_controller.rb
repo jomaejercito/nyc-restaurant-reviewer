1d5f613e27be4dd189b3fe9d2906274007e24220
Joma Ejercito <jomaejercito@gmail.com>
2018-11-06 02:25:17 -0500

    Tried to create the create method in reviews controller but it's not working. Gonna figure it out.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,16 +5,27 @@ class ReviewsController < ApplicationController
     @reviews = @user.reviews.all
   end
 
+  def show
+    @user = User.find_by(id: params[:user_id])
+    @review = Review.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
     @user = current_user
     @restaurant = Restaurant.find_by(id: params[:restaurant_id])
     @review = Review.new
   end
 
-  def show
-    @user = User.find_by(id: params[:user_id])
-    @review = Review.find(params[:id])
+  def create
+    @user = current_user
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to user_reviews_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
